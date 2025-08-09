661595012f4f5c47226d0ff821baa23b77130478
sajjad <saj.maleki@gmail.com>
2025-08-10 00:09:43 +0330

    chore : modular jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
